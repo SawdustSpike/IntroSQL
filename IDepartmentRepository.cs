0350b5522acc5fa6948a90954d454b36c2fa22fe
Mike Cowell <spike@spikeandbetty.com>
2022-11-17 12:06:23 -0500

    Dapper is go.

---
+++ IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSQL
+
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments();
+
+        public void InsertDepartment()
+        {
+           
+        }
+    }
+}
